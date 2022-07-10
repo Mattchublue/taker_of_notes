bbb3a7d8151418026c1de07b19c3f467a976f26a
Matthew <mwhiteman83@gmail.com>
2022-07-09 18:00:37 -0600

    deploying heroku

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 var express = require("express");
+var viewroutes = require("./routing/view-routes")
+var controllerroutes =require("./routing/controller-routes")
 
 // 2. I'm creating an 'express' server called app
 var app = express();
 
 // 3. Sets a port or run at 7000, Later listener will listen this
-var PORT = process.env.PORT || 7000;
+var PORT = process.env.PORT || 3000;
 
 //4. Use a middleware to parse the JSON data
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 //what folder the browser can see
 app.use(express.static("public"));
+app.use("/api", controllerroutes);
 
 //5. Bring routes
-// require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
+app.use("/", viewroutes);
 //6. Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
